3e08e20f87c4a1ef3a14e94cc53de80f1b18fb18
derrick b <dbozich1@gmail.com>
2018-09-24 01:46:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ function setThisWithCall(fn, thisValue, arg){
   return result = fn.call(thisValue, arg);
 }
 
+function setThisWithApply(fn, thisValue, args){
+  return result = fn.apply(thisValue, arg);
+}
+
 // function (){ return {thisValue: this, arguments: Array.from(arguments)} }
