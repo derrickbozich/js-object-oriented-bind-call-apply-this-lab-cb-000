532223d7cbfc46a0dd196c4e609ca86c38cdd7a5
derrick b <dbozich1@gmail.com>
2018-09-24 01:55:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,9 @@ function setThisWithApply(fn, thisValue, args){
   return result = fn.apply(thisValue, args);
 }
 
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  let copy = functionToBeCopied;
+  return copy.call(thisValue);
+}
+
 // function (){ return {thisValue: this, arguments: Array.from(arguments)} }
